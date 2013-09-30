e3a240ee793fd12f977719231a316ae719e4cc5b
Yule <paul.brennan@ahc.uk.com>
2013-10-01 00:58:42 +0100

    full house tie break and slight refactor

--- ranking/lib/hand_type/full_house.rb
+++ ranking/lib/hand_type/full_house.rb
@@ -3,14 +3,15 @@ module HandType
 
     def handles?
       return false unless n_of_a_kind? 3
-
-      value = highest_same_value 3
-
-      highest_same_value_except(2, value) > 0
+      second_value > 0
     end
 
     def rank
       6
+    end
+
+    def second_value
+      highest_same_value_except(2, value)
     end
 
     def value
--- ranking/rspec/hand_spec.rb
+++ ranking/rspec/hand_spec.rb
@@ -176,6 +176,11 @@ describe 'Hand' do
           should_defeat hand('9 of Diamonds', '9 of Hearts', '9 of Spades', 'King of Spades', 'King of Clubs')
     end
 
+    it "should rank higher full house based on pair if trips is same" do
+      hand('10 of Diamonds', '10 of Hearts', '10 of Spades', 'Jack of Spades', 'Jack of Clubs').
+          should_defeat hand('10 of Diamonds', '10 of Hearts', '10 of Spades', '5 of Spades', '5 of Clubs')
+    end
+
     it 'should rank four of a kind over full house' do
       hand('10 of Diamonds', '10 of Hearts', '10 of Spades', '10 of Clubs').
           should_defeat hand('Jack of Diamonds', 'Jack of Hearts', 'Jack of Spades', 'King of Spades', 'King of Clubs')
