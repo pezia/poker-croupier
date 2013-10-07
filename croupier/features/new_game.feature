455c8db3af718698856cad2f33832cd28ab6b51f
devill <devillmeister@gmail.com>
2013-10-07 07:57:25 +0200

    Added more steps to the first feature to test the first card dealing story

--- croupier/features/new_game.feature
+++ croupier/features/new_game.feature
@@ -1,10 +1,16 @@
 Feature: new game of poker
 
-  Scenario: Two players are registered, and they get notified about each other
+  Scenario: Two players are registered, and they get notified about each other. After that they get their hole cards
+    and the small blind player - Bob - folds. The first round is over and the winner is the big blind player, Adam.
 
-    Given The croupier is ready for a game
+    Given the croupier is ready for a game
     And "Adam" is a player
     And "Bob" is a player
+    And the deck contains the following cards:
+      | 5 | Diamonds |
+      | 6 | Hearts   |
+      | 7 | Spades   |
+      | 8 | Clubs    |
     When I start a sit and go
     Then "Adam" gets the following list of players:
       | Adam |
@@ -12,3 +18,9 @@ Feature: new game of poker
     And "Bob" gets the following list of players:
       | Adam |
       | Bob  |
+    And "Adam" gets the following hole cards:
+      | 5 | Diamonds |
+      | 7 | Spades   |
+    And "Bob" gets the following hole cards:
+      | 6 | Hearts   |
+      | 8 | Clubs    |
--- croupier/features/step_definitions/poker_steps.rb
+++ croupier/features/step_definitions/poker_steps.rb
@@ -1,20 +1,42 @@
 require_relative '../../croupier'
 
-Given(/^The croupier is ready for a game$/) do
+
+
+Given(/^the croupier is ready for a game$/) do
   Croupier::TestFramework::TestCroupier.new_instance
 end
+
+
 
 Given(/^"([^"]*)" is a player$/) do |player_name|
   croupier = Croupier::TestFramework::TestCroupier.instance
   croupier.register_player Croupier::TestFramework::FakePlayer.new(player_name)
 end
+
+
+
 When(/^I start a sit and go$/) do
   Croupier::TestFramework::TestCroupier.instance.start_sit_and_go
 end
+
+
+
 Then(/^"([^"]*)" gets the following list of players:$/) do |player_name, received_statuses|
   player = Croupier::TestFramework::FakePlayerRegistry.instance.find(player_name)
   received_statuses.raw.each do |competitor_name|
     competitor = Croupier::TestFramework::FakePlayerRegistry.instance.find(competitor_name.first)
     player.next_message.should == [:competitor_status, competitor]
   end
+end
+
+
+
+When(/^the deck contains the following cards:$/) do |table|
+
+end
+
+
+
+When(/^"([^"]*)" gets the following hole cards:$/) do |player_name, table|
+
 end
