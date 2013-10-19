d76a1e857db1cfb8c554ec7f82abdbe5125c3069
Attila M. Magyar <attila.m.magyar@gmail.com>
2013-10-19 14:10:30 +0200

    betting_spec: clean up the work-in-progress specs

--- croupier/spec/game_steps/betting_spec.rb
+++ croupier/spec/game_steps/betting_spec.rb
@@ -1,31 +1,45 @@
 require_relative '../spec_helper'
 
 describe Croupier::GameSteps::Betting do
+  before :each do
+    @player1 = SpecHelper::FakePlayer.new
+    @game_state = SpecHelper::MakeGameState.with players: [@player1]
+  end
 
   it "should request a bet from the player in action" do
-    game_state = SpecHelper::MakeGameState.with players: [SpecHelper::FakePlayer.new]
+    @player1.should_bet 0
+    run
+  end
 
-    game_state.players[0].should_receive(:bet_request).and_return(0)
-
-    Croupier::GameSteps::Betting.new.run(game_state)
+  def run()
+    Croupier::GameSteps::Betting.new.run(@game_state)
   end
 
   it "should transfer a non zero bet to the pot" do
-    game_state = SpecHelper::MakeGameState.with players: [SpecHelper::FakePlayer.new]
-
-    game_state.players[0].should_receive(:bet_request).and_return(20)
-
-    Croupier::GameSteps::Betting.new.run(game_state)
-
-    game_state.pot.should == 20
+    @player1.should_bet 20
+    run
+    @game_state.pot.should == 20
   end
 
-  it "should ask the second player after the first player" do
-    game_state = SpecHelper::MakeGameState.with players: [SpecHelper::FakePlayer.new, SpecHelper::FakePlayer.new]
+  context "at least two players" do
 
-    game_state.players[0].should_receive(:bet_request).and_return(0)
-    game_state.players[1].should_receive(:bet_request).and_return(0)
+    before :each do
+      @player2 = SpecHelper::FakePlayer.new
+      @game_state.register_player @player2
+    end
 
-    Croupier::GameSteps::Betting.new.run(game_state)
+    it "should ask the second player after the first player" do
+      @player1.should_bet 0
+      @player2.should_bet 0
+      run
+    end
+
+    it "should transfer non zero bets to the pot" do
+      @player1.should_bet 20
+      @player2.should_bet 20
+      run
+      @game_state.pot.should == 40
+    end
+
   end
 end
--- croupier/spec/spec_helper/fake_player.rb
+++ croupier/spec/spec_helper/fake_player.rb
@@ -1,2 +1,5 @@
 class SpecHelper::FakePlayer < SpecHelper::DummyClass
+  def should_bet(bet)
+    should_receive(:bet_request).and_return(bet)
+  end
 end
