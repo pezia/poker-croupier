86efa1d4373bb4a88a1b3e1260a7505b4689957e
devill <devillmeister@gmail.com>
2013-10-30 22:39:13 +0100

    Removed duplication between flop and deal community card

--- croupier/lib/game_steps/flop.rb
+++ croupier/lib/game_steps/flop.rb
@@ -1,10 +1,7 @@
 class Croupier::GameSteps::Flop < Croupier::GameSteps::Base
   def run
     3.times do
-      card = game_state.deck.next_card!
-      game_state.each_player_and_spectator do |observer|
-        observer.community_card(card)
-      end
+      Croupier::GameSteps::DealCommunityCard.new(game_state).run
     end
   end
 
