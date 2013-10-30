b9d94cdb28365261d8ee6d6aaead38e247aa6026
devill <devillmeister@gmail.com>
2013-10-30 21:42:59 +0100

    Refactored flop, to extract turn and river steps from it

--- croupier/lib/game_steps/flop.rb
+++ croupier/lib/game_steps/flop.rb
@@ -1,10 +1,10 @@
 class Croupier::GameSteps::Flop < Croupier::GameSteps::Base
   def run
-    cards = Array.new()
-    3.times { cards.push(game_state.deck.next_card!) }
-
-    game_state.each_player_and_spectator do |observer|
-      cards.each { |card| observer.community_card(card) }
+    3.times do
+      card = game_state.deck.next_card!
+      game_state.each_player_and_spectator do |observer|
+        observer.community_card(card)
+      end
     end
   end
 
