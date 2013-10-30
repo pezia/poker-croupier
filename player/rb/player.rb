0b4b362421eda8377362400e827f2fa107c0552a
devill <devillmeister@gmail.com>
2013-10-30 23:04:47 +0100

    Removed dead comments

--- player/rb/player.rb
+++ player/rb/player.rb
@@ -7,10 +7,3 @@ require 'player_strategy'
 module PlayerStrategy
   autoload :Handler, 'lib/handler'
 end
-
-#port = ARGV[1]
-#
-#processor = API::PlayerStrategy::Processor.new(PlayerStrategy::Handler.new())
-#transport = Thrift::ServerSocket.new(port)
-#server = Thrift::ThreadPoolServer.new(processor, transport)
-#server.serve()
