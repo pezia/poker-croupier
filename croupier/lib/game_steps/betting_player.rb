18e22d2d6dfe1b606cf72478c420dfac0795ebc4
devill <devillmeister@gmail.com>
2013-11-07 23:54:21 +0100

    Renamed some variables

--- croupier/lib/game_steps/betting_player.rb
+++ croupier/lib/game_steps/betting_player.rb
@@ -1,8 +1,8 @@
 class Croupier::GameSteps::BettingPlayer
 
-  def initialize(game_state, index)
-    @game_state, @index = game_state, index
-    @player = game_state.game_state.players[index]
+  def initialize(betting_state, index)
+    @betting_state, @index = betting_state, index
+    @player = betting_state.game_state.players[index]
 
     @total_bet = 0
   end
@@ -28,26 +28,26 @@ class Croupier::GameSteps::BettingPlayer
   private
 
   def call?
-    @total_bet == @game_state.current_buy_in
+    @total_bet == @betting_state.current_buy_in
   end
 
   def handle_call(bet)
-    bet_type = (@game_state.current_buy_in == 0) ? :check : :call
-    @game_state.game_state.transfer_bet @player, bet, bet_type
+    bet_type = (@betting_state.current_buy_in == 0) ? :check : :call
+    @betting_state.game_state.transfer_bet @player, bet, bet_type
   end
 
   def raise?
-    @total_bet > @game_state.current_buy_in
+    @total_bet > @betting_state.current_buy_in
   end
 
   def handle_raise(bet)
-    @game_state.current_buy_in = @total_bet
-    @game_state.last_raise = @index
-    @game_state.game_state.transfer_bet @player, bet, :raise
+    @betting_state.current_buy_in = @total_bet
+    @betting_state.last_raise = @index
+    @betting_state.game_state.transfer_bet @player, bet, :raise
   end
 
   def handle_fold
-    @game_state.game_state.transfer_bet @player, 0, :fold
+    @betting_state.game_state.transfer_bet @player, 0, :fold
     @player.fold
   end
 end
