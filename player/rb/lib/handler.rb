b1c0b1b0a746f65fa82febf8aa95a521c08f8b0e
devill <devillmeister@gmail.com>
2013-11-17 19:10:38 +0100

    Removed logging from ruby player service

--- player/rb/lib/handler.rb
+++ player/rb/lib/handler.rb
@@ -5,19 +5,15 @@ class PlayerStrategy::Handler
   end
 
   def competitor_status(competitor)
-    p competitor
   end
 
   def hole_card(card)
-    p "hole card #{card.name} (#{card.value}, #{card.suit})"
   end
 
   def community_card(card)
-    p "community card #{card}"
   end
 
   def bet(competitor, bet)
-    p competitor, bet
   end
 
   def bet_request
@@ -25,6 +21,5 @@ class PlayerStrategy::Handler
   end
 
   def winner(competitor)
-    p competitor
   end
 end
