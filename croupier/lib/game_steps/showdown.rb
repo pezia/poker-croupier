74ef70695838c98abf2074f564b98e50a2458b1e
devill <devillmeister@gmail.com>
2013-11-17 20:26:16 +0100

    Refactored showdown

--- croupier/lib/game_steps/showdown.rb
+++ croupier/lib/game_steps/showdown.rb
@@ -4,48 +4,47 @@ require 'ranking/hand'
 
 class Croupier::GameSteps::Showdown < Croupier::GameSteps::Base
   def run
-    winners = find
-    announce(winners)
-    award(winners)
+    find_winner
+    announce
+    award
   end
 
   private
 
-  def find
-    winners = []
-    best_hand = Ranking::Hand.new
+  def find_winner
+    @winners = []
+    @best_hand = Ranking::Hand.new
 
     game_state.each_player do |player|
-      unless player.active?
-        next
-      end
-
-      hand = Ranking::Hand.new *game_state.community_cards, *player.hole_cards
-      if best_hand.defeats? hand
-        next
-      end
-
-      winners = [] if hand.defeats? best_hand
-
-      winners << player
-      best_hand = hand
+      examine_cards_of player
     end
-    winners
   end
 
-  def announce(winners)
+  def examine_cards_of(player)
+    return unless player.active?
+
+    hand = Ranking::Hand.new *game_state.community_cards, *player.hole_cards
+    return if @best_hand.defeats? hand
+
+    @winners = [] if hand.defeats? @best_hand
+
+    @winners << player
+    @best_hand = hand
+  end
+
+  def announce
     game_state.each_observer do |player|
-      winners.each do |winner|
+      @winners.each do |winner|
         player.winner winner
       end
     end
   end
 
-  def award(winners)
+  def award
     total_pot = game_state.pot
-    remainder = total_pot % winners.length
-    winners.each_with_index do |winner, index|
-      game_state.transfer winner, -(total_pot / winners.length).floor - (index < remainder ? 1 : 0)
+    remainder = total_pot % @winners.length
+    @winners.each_with_index do |winner, index|
+      game_state.transfer winner, -(total_pot / @winners.length).floor - (index < remainder ? 1 : 0)
     end
   end
 end
