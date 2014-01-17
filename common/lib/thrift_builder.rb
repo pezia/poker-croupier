6e2d7c5ebe8fb76de8f61517ff6da047df37e774
devill <devillmeister@gmail.com>
2014-01-17 21:29:41 +0100

    Host name 'localhost' is passed to the transport

--- common/lib/thrift_builder.rb
+++ common/lib/thrift_builder.rb
@@ -2,7 +2,7 @@
 class ThriftBuilder
   def self.server(processor, handler, port)
     processor = processor.new(handler.new())
-    transport = Thrift::ServerSocket.new(port)
+    transport = Thrift::ServerSocket.new('localhost', port)
     Thrift::ThreadPoolServer.new(processor, transport)
   end
 end
