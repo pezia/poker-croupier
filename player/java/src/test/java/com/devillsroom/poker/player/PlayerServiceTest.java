12e81a4a3f0cf0e0ebddb171474c8e5c2a0516b8
Peter Szanto <github@szantocsalad.hu>
2014-01-21 02:04:44 +0100

    renamed script

--- player/java/src/test/java/com/devillsroom/poker/player/PlayerServiceTest.java
+++ player/java/src/test/java/com/devillsroom/poker/player/PlayerServiceTest.java
@@ -2,23 +2,28 @@ package com.devillsroom.poker.player;
 
 import org.junit.Test;
 
+import java.io.IOException;
+
 public class PlayerServiceTest {
 
     @Test
-    public void testStart2Players() throws InterruptedException {
+    public void testStart2Players() throws InterruptedException, IOException {
 
-        startPlayerThread("Java Player 1", 9200, false);
-        startPlayerThread("Java Player 2", 9201, true);
+        startPlayerThread("Java Player 1", 9200);
+        startPlayerThread("Java Player 2", 9201);
 
+        System.out.println(execCmd("bundle exec ruby ../../croupier/scripts/integration_test_external_players.rb"));
     }
 
-    private void startPlayerThread(String name, int port, boolean join) throws InterruptedException {
+    public static String execCmd(String cmd) throws java.io.IOException {
+        Process p = Runtime.getRuntime().exec(cmd);
+        java.util.Scanner s = new java.util.Scanner(p.getErrorStream()).useDelimiter("\\A");
+        return s.hasNext() ? s.next() : "";
+    }
+
+    private void startPlayerThread(String name, int port) throws InterruptedException {
         Thread t = new Thread(new PlayerService(name, port));
         t.start();
-
-        if (join) {
-            t.join();
-        }
     }
 
 
