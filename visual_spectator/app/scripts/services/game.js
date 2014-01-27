7784029d29a293e87204ed95a40fafd422d3656b
Zsolt Petrik <pezia@pezia.hu>
2014-01-27 22:11:43 +0100

    Some controls are now functioning

--- visual_spectator/app/scripts/services/game.js
+++ visual_spectator/app/scripts/services/game.js
@@ -4,7 +4,7 @@ var gameServices = angular.module('visualSpectatorApp');
 
 gameServices.factory('Game', ['$resource',
     function($resource) {
-        return $resource('http://localhost/poker-croupier/log/:gameName.json', {}, {
+        return $resource('log/:gameName.json', {}, {
             get: {method: 'GET', params: {gameName: 'gameName'}, isArray: true}
         });
     }]);
